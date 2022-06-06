2fc302a2409031d1d09d3b7e28cf39fa504a418a
Ritik Singh <ritikbandwal@gmail.com>
2022-06-06 15:00:50 +0530

    added imgs and homescreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,28 +1,47 @@
-import React from 'react'
-import { View, Text, StyleSheet, Image } from 'react-native'
-import MainPageCard from '../components/MainPageCard'
-import Header from '../layout/Header'
-import { goBack } from '../navigation/utils/RootNavigation'
+import React from 'react';
+import {View, Text, StyleSheet, Image, ImageBackground} from 'react-native';
+import MainPageCard from '../components/MainPageCard';
+import Header from '../layout/Header';
+import {goBack} from '../navigation/utils/RootNavigation';
 
-
-
-
-export const HomeScreen = (props) => {
-
-    const { container } = styles
-    return (
-        <View style={container}>
-            <Header />
-            <View>
-                <Text>Home Screen</Text>
-            </View>
-        </View>
-    )
-}
-
+export const HomeScreen = props => {
+  const {container} = styles;
+  return (
+    <View style={container}>
+      <Header />
+      <View>
+        <ImageBackground
+          source={require('../assets/images/homepage1.png')}
+          style={styles.backgroundImage}>
+          <View>
+            <Text
+              style={{
+                paddingRight: 96,
+                paddingBottom: 16,
+                fontSize: 24,
+                fontWeight: 'bold',
+              }}>
+              나만 알고싶은{'\n'}힙한 캠핑장 지금 공개
+            </Text>
+          </View>
+          <View>
+            <Text style={{paddingRight: 92, fontSize: 14, fontWeight: '300'}}>
+              바퀴달린집 출연 캠핑장 최대 할인가로{'\n'}떠나보면 어때요?
+            </Text>
+          </View>
+        </ImageBackground>
+      </View>
+      <View></View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-
-    }
-})
+  container: {},
+  backgroundImage: {
+    display: 'flex',
+    height: 352,
+    paddingLeft: 60,
+    paddingTop: 72,
+  },
+});

0a5fbc3affddd0eef8c778df54b2382d94737f1a
BadalSherpa <badalsherpa7@gmail.com>
2022-06-14 23:30:23 +0530

    new package and usestate demo

--- src/screens/HeartScreen.js
+++ src/screens/HeartScreen.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
+import { set } from 'react-hook-form'
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 import Header from '../layout/Header'
 import { navigateTo } from '../navigation/utils/RootNavigation'
@@ -10,11 +11,34 @@ import { navigateTo } from '../navigation/utils/RootNavigation'
 export const HeartScreen = (props) => {
 
     const { container } = styles
+    const count = useRef(0);
+
+    const expensiveFunc = () => {
+        console.log("EXPENSIVE")
+    }
+
+    console.log(count);
+    const increment = () => {
+        count.current = count.current + 1;
+        console.log("INSIDE", count);
+    }
+
+    const decrement = () => {
+        count.current = count.current + 1;
+    }
+
+    console.log("IS MOUNTING");
+    expensiveFunc()
+
     return (
         <View style={container}>
             <Header headerContent={{}} />
             <TouchableOpacity onPress={() => { navigateTo("Chat") }}>
-                <Text>HeartScreen</Text>
+                <View style={{ display: "flex", flexDirection: "row", justifyContent: "space-around" }}>
+                    <Text onPress={() => { increment() }}>+</Text>
+                    <Text>{count.current}</Text>
+                    <Text onPress={() => { decrement() }}>-</Text>
+                </View>
             </TouchableOpacity>
         </View>
     )

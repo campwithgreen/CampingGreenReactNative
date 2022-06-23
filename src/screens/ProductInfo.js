e2a51fe63c7ed7def51b5f0d1a02b7a83ba124e6
Ritik Singh <ritikbandwal@gmail.com>
2022-06-23 22:20:03 +0530

    Tab View

--- src/screens/ProductInfo.js
+++ src/screens/ProductInfo.js
@@ -9,16 +9,12 @@ import {
   ImageBackground,
 } from 'react-native';
 import Header from '../layout/Header';
-import HomeScreenDetail from '../components/HomeScreenDetail';
 import {
   heightPercentageToDP as hp,
   widthPercentageToDP as wp,
 } from 'react-native-responsive-screen';
 import {RFPercentage} from 'react-native-responsive-fontsize';
 import Carousel from '../components/Carousel';
-import {useDispatch, useSelector} from 'react-redux';
-import {login} from '../redux/actions/oauth';
-import {navigateTo} from '../navigation/utils/RootNavigation';
 import {Dimensions, StatusBar} from 'react-native';
 import {TabView, SceneMap} from 'react-native-tab-view';
 
@@ -67,7 +63,6 @@ const imageInfo = [
 
 export const ProductInfo = props => {
   const {container} = styles;
-  const dispatch = useDispatch();
   const item = props.route.params;
   console.log('HELLORitik', item);
 
@@ -174,104 +169,36 @@ export const ProductInfo = props => {
               </View>
             </View>
           </View>
-          <View>
-            <TabView
-              navigationState={{index, routes}}
-              renderScene={renderScene}
-              onIndexChange={setIndex}
-              initialLayout={initialLayout}
-              style={styles.container}
-            />
-          </View>
-          <View>
-            <View style={{paddingBottom: hp('3%')}}>
-              <Text
-                style={{
-                  color: '#1B1D1F',
-                  fontSize: RFPercentage(3),
-                  fontWeight: 'bold',
-                }}>
-                상품{'\n'}상세 정보
-              </Text>
-            </View>
-          </View>
         </View>
-        <ImageBackground
-          source={require('../assets/images/tentinfo.png')}
-          style={{height: hp('28%')}}
-        />
-        <View
-          style={{display: 'flex', alignItems: 'center', paddingTop: hp('3%')}}>
-          <View
-            style={{
-              backgroundColor: '#26282B',
-              padding: wp('1.5%'),
-              width: wp('60%'),
-            }}>
-            <Text
-              style={{
-                color: '#FFF',
-                fontSize: RFPercentage(2.5),
-                fontWeight: 'bold',
-              }}>
-              {item?.detail1?.detail1Title}
-            </Text>
-          </View>
-          <View
-            style={{
-              backgroundColor: 'white',
-              marginTop: hp('3%'),
-              padding: wp('2%'),
-              width: wp('60%'),
-              height: hp('20%'),
-            }}>
-            {Object.keys(item?.detail1?.detail1Value).map(key => {
-              return (
-                <View
-                  key={key}
-                  style={{
-                    display: 'flex',
-                    flexDirection: 'row',
-                  }}>
-                  <Text
-                    style={{
-                      color: '#454C53',
-                      fontSize: RFPercentage(2),
-                      marginBottom: hp('.5%'),
-                      width: wp('30%'),
-                    }}>
-                    {key}
-                  </Text>
-                  <Text
-                    style={{
-                      color: '#454C53',
-                      fontSize: RFPercentage(2),
-                      marginBottom: hp('.5%'),
-                    }}>
-                    {item?.detail1?.detail1Value[key]}
-                  </Text>
-                </View>
-              );
-            })}
-          </View>
+        <View>
+          <TabView
+            navigationState={{index, routes}}
+            renderScene={renderScene}
+            onIndexChange={setIndex}
+            initialLayout={initialLayout}
+            style={styles.container}
+          />
         </View>
-        <View style={{marginHorizontal: wp('5%')}}>
-          <View style={{paddingTop: hp('10%')}}>
+        <View>
+          <View style={{paddingBottom: hp('3%'), marginHorizontal: wp('5%')}}>
             <Text
               style={{
                 color: '#1B1D1F',
                 fontSize: RFPercentage(3),
                 fontWeight: 'bold',
               }}>
-              상품 {'\n'}상세 구성
+              상품{'\n'}상세 정보
             </Text>
           </View>
+          <ImageBackground
+            source={require('../assets/images/tentinfo.png')}
+            style={{height: hp('28%')}}
+          />
           <View
             style={{
               display: 'flex',
               alignItems: 'center',
               paddingTop: hp('3%'),
-              paddingBottom: hp('5%'),
             }}>
             <View
               style={{
@@ -285,7 +212,7 @@ export const ProductInfo = props => {
                   fontSize: RFPercentage(2.5),
                   fontWeight: 'bold',
                 }}>
-                {item?.details2?.detail2Title}
+                {item?.detail1?.detail1Title}
               </Text>
             </View>
             <View
@@ -296,7 +223,7 @@ export const ProductInfo = props => {
                 width: wp('60%'),
                 height: hp('20%'),
               }}>
-              {Object.keys(item?.details2?.detail2Value).map(key => {
+              {Object.keys(item?.detail1?.detail1Value).map(key => {
                 return (
                   <View
                     key={key}
@@ -311,56 +238,216 @@ export const ProductInfo = props => {
                         marginBottom: hp('.5%'),
                         width: wp('30%'),
                       }}>
-                      {item?.details2?.detail2Value[key]}
+                      {key}
+                    </Text>
+                    <Text
+                      style={{
+                        color: '#454C53',
+                        fontSize: RFPercentage(2),
+                        marginBottom: hp('.5%'),
+                      }}>
+                      {item?.detail1?.detail1Value[key]}
                     </Text>
                   </View>
                 );
               })}
             </View>
           </View>
+          <View style={{marginHorizontal: wp('5%')}}>
+            <View style={{paddingTop: hp('10%')}}>
+              <Text
+                style={{
+                  color: '#1B1D1F',
+                  fontSize: RFPercentage(3),
+                  fontWeight: 'bold',
+                }}>
+                상품 {'\n'}상세 구성
+              </Text>
+            </View>
+            <View
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                paddingTop: hp('3%'),
+                paddingBottom: hp('5%'),
+              }}>
+              <View
+                style={{
+                  backgroundColor: '#26282B',
+                  padding: wp('1.5%'),
+                  width: wp('60%'),
+                }}>
+                <Text
+                  style={{
+                    color: '#FFF',
+                    fontSize: RFPercentage(2.5),
+                    fontWeight: 'bold',
+                  }}>
+                  {item?.details2?.detail2Title}
+                </Text>
+              </View>
+              <View
+                style={{
+                  backgroundColor: 'white',
+                  marginTop: hp('3%'),
+                  padding: wp('2%'),
+                  width: wp('60%'),
+                  height: hp('20%'),
+                }}>
+                {Object.keys(item?.details2?.detail2Value).map(key => {
+                  return (
+                    <View
+                      key={key}
+                      style={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                      }}>
+                      <Text
+                        style={{
+                          color: '#454C53',
+                          fontSize: RFPercentage(2),
+                          marginBottom: hp('.5%'),
+                          width: wp('30%'),
+                        }}>
+                        {item?.details2?.detail2Value[key]}
+                      </Text>
+                    </View>
+                  );
+                })}
+              </View>
+            </View>
+          </View>
+          {imageInfo.map(item => {
+            return (
+              <View>
+                <ImageBackground
+                  source={item.img}
+                  style={{height: hp('28%')}}
+                />
+                <Text
+                  style={{
+                    color: '#1B1D1F',
+                    fontSize: RFPercentage(2.5),
+                    fontWeight: 'bold',
+                    marginHorizontal: wp('5%'),
+                    marginVertical: hp('2.5%'),
+                  }}>
+                  {item.imgtext}
+                </Text>
+              </View>
+            );
+          })}
         </View>
-        <ImageBackground
-          source={require('../assets/images/tentinfo1.png')}
-          style={{height: hp('28%')}}
-        />
-        <Text
-          style={{
-            color: '#1B1D1F',
-            fontSize: RFPercentage(2.5),
-            fontWeight: 'bold',
-            marginHorizontal: wp('5%'),
-            marginVertical: hp('2.5%'),
-          }}>
-          1~2인용 비빅텐트
-        </Text>
-        <ImageBackground
-          source={require('../assets/images/tentinfo2.png')}
-          style={{height: hp('28%')}}
-        />
-        <Text
-          style={{
-            color: '#1B1D1F',
-            fontSize: RFPercentage(2.5),
-            fontWeight: 'bold',
-            marginHorizontal: wp('5%'),
-            marginVertical: hp('2.5%'),
-          }}>
-          1~2인용 비빅텐트
-        </Text>
-        <ImageBackground
-          source={require('../assets/images/tentinfo3.png')}
-          style={{height: hp('28%')}}
-        />
-        <Text
-          style={{
-            color: '#1B1D1F',
-            fontSize: RFPercentage(2.5),
-            fontWeight: 'bold',
-            marginHorizontal: wp('5%'),
-            marginVertical: hp('2.5%'),
-          }}>
-          1~2인용 비빅텐트
-        </Text>
+        <View style={{marginHorizontal: wp('5%')}}>
+          <View style={{paddingBottom: hp('3%')}}>
+            <Text
+              style={{
+                color: '#1B1D1F',
+                fontSize: RFPercentage(3),
+                fontWeight: 'bold',
+              }}>
+              예약 {'\n'}유의사항
+            </Text>
+          </View>
+          <View style={{paddingBottom: hp('7%')}}>
+            <Text
+              style={{
+                color: '#1B1D1F',
+                fontSize: RFPercentage(2),
+                fontWeight: 'bold',
+                paddingBottom: hp('2%'),
+              }}>
+              취소/ 환불규정
+            </Text>
+            <Text
+              style={{
+                color: '#1B1D1F',
+                fontSize: RFPercentage(1.8),
+              }}>
+              예약한 용품 대여 취소는 택배발송 전까지 가능하며, 용품 발송 후에는
+              주문취소가 불가능합니다. 용품 대여 취소는 마이페이지- 캠핑용품
+              대여내역 조회에서 가능합니다. 기타문의 사항은 고객센터로
+              문의바랍니다.
+            </Text>
+          </View>
+          <View style={{paddingBottom: hp('5%')}}>
+            <Text
+              style={{
+                color: '#1B1D1F',
+                fontSize: RFPercentage(3),
+                fontWeight: 'bold',
+              }}>
+              배송 {'\n'}안내사항
+            </Text>
+          </View>
+          <View style={{paddingBottom: hp('5%')}}>
+            <Text
+              style={{
+                color: '#1B1D1F',
+                fontSize: RFPercentage(2),
+                fontWeight: 'bold',
+                paddingBottom: hp('2%'),
+              }}>
+              배송조회 안내
+            </Text>
+            <Text
+              style={{
+                color: '#1B1D1F',
+                fontSize: RFPercentage(1.8),
+              }}>
+              결제완료 후 배송까지 최소 2~3일이 소요될 수 있습니다.
+              마이페이지-캠핑용품 대여내역조회에서 배송현황을 확인하실 수
+              있습니다.
+            </Text>
+          </View>
+          <View style={{paddingBottom: hp('5%')}}>
+            <Text
+              style={{
+                color: '#1B1D1F',
+                fontSize: RFPercentage(2),
+                fontWeight: 'bold',
+                paddingBottom: hp('2%'),
+              }}>
+              대여장비 반납방법
+            </Text>
+            <Text
+              style={{
+                color: '#1B1D1F',
+                fontSize: RFPercentage(1.8),
+              }}>
+              대여용품 반납은 캠핑그린에서 반납예약을 해드립니다. 대여종료일
+              다음날, 배송받으셨던 주소로 택배기사님이 방문하여 수거합니다.
+            </Text>
+          </View>
+          <View style={{paddingBottom: hp('4%')}}>
+            <Text
+              style={{
+                color: '#1B1D1F',
+                fontSize: RFPercentage(2),
+                fontWeight: 'bold',
+                paddingBottom: hp('2%'),
+              }}>
+              대여장비 반납시 유의사항
+            </Text>
+            <Text
+              style={{
+                color: '#1B1D1F',
+                fontSize: RFPercentage(1.8),
+              }}>
+              장비는 배송시 보내드린 박스에 용품을 넣으신 후 보내주세요. 장비
+              반납의 책임은 전적으로 고객에게 있으며 주문하셨던 장비 중 누락된
+              용품이 없는지 확인해주세요. 반납시 누락된 상품으로 다시 택배를
+              이용할 경우 배송비를 추가로 부담하셔야합니다. 캠핑그린의
+              캠핑용품은 다른 고객들도 함께 사용하는 대여품입니다. {'\n\n\n'}
+              고품질의 서비스를 지속적으로 제공하기 위해서 깨끗이 사용하시고
+              사용 후에도 반드시 청소해서 반납해주시길 바랍니다. {'\n\n\n'}
+              <Text style={{fontWeight: 'bold'}}>
+                대여지와 배송지가 다를 경우, 택배사에 직접 전화하여 예약하시거나
+                인터넷을 이용해주세요.
+              </Text>
+            </Text>
+          </View>
+        </View>
       </ScrollView>
     </View>
   );

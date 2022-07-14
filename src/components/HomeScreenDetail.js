c89ac19a9091bf4acfac1e99302f284f03888d59
campwithgreen <campwithgreen@gmail.com>
2022-07-14 20:24:13 +0900

    fix

--- src/components/HomeScreenDetail.js
+++ src/components/HomeScreenDetail.js
@@ -4,96 +4,97 @@ import {
   View,
   Image,
   ImageBackground,
-  TouchableOpacity,
+  Pressable,
 } from 'react-native';
 import React from 'react';
 import {
   heightPercentageToDP as hp,
   widthPercentageToDP as wp,
 } from 'react-native-responsive-screen';
-import { RFPercentage } from 'react-native-responsive-fontsize';
-import { navigateTo } from '../navigation/utils/RootNavigation';
+import {RFPercentage} from 'react-native-responsive-fontsize';
+import {navigateTo} from '../navigation/utils/RootNavigation';
 
 export default function HomeScreenDetail(props) {
-  const { HomeScreenDetailData } = props;
-  const { container } = styles;
+  const {HomeScreenDetailData} = props;
+  const {container} = styles;
   return (
     <View style={container}>
-      <ImageBackground
-        source={HomeScreenDetailData.image}
-        style={{
-          minHeight: hp('27.6%'),
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'space-between',
+      <Pressable
+        onPress={() => {
+          if (HomeScreenDetailData.id === 10) {
+            navigateTo('HomeScreenDetail1', {
+              imageId: HomeScreenDetailData.id,
+            });
+          } else {
+            navigateTo('RoomScreen');
+          }
         }}>
-        <View
+        <ImageBackground
+          source={HomeScreenDetailData.image}
           style={{
-            paddingLeft: wp('5%'),
-          }}>
-          <View>
-            <Text
-              style={{
-                color: '#E8EBED',
-                fontSize: RFPercentage(2),
-                paddingTop: hp('2%'),
-              }}>
-              {HomeScreenDetailData.heading}
-            </Text>
-          </View>
-          <View>
-            <Text
-              style={{
-                color: '#FFFFFF',
-                fontSize: RFPercentage(3.2),
-                paddingTop: hp('1.5%'),
-                paddingBottom: hp('1.5%'),
-              }}>
-              {HomeScreenDetailData.heading2}
-            </Text>
-          </View>
-          <View>
-            <Text
-              style={{
-                color: '#FFFFFF',
-                fontSize: RFPercentage(2),
-              }}>
-              {HomeScreenDetailData.heading3}
-            </Text>
-          </View>
-        </View>
-        <View
-          style={{
+            minHeight: hp('27.6%'),
             display: 'flex',
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'flex-end',
-            paddingRight: wp('5%'),
-            paddingBottom: hp('1%')
+            flexDirection: 'column',
+            justifyContent: 'space-between',
           }}>
-          <Text
+          <View
             style={{
-              color: '#F7F8F9',
-              fontSize: RFPercentage(1.8),
               paddingLeft: wp('5%'),
-              paddingRight: wp('1%'),
             }}>
-            {HomeScreenDetailData.heading4}
-          </Text>
-          <TouchableOpacity
-            onPress={() => {
-              if (HomeScreenDetailData.id === 10) {
-                navigateTo('HomeScreenDetail1', {
-                  imageId: HomeScreenDetailData.id,
-                });
-              } else {
-                navigateTo("RoomScreen");
-              }
+            <View>
+              <Text
+                style={{
+                  color: '#E8EBED',
+                  fontSize: RFPercentage(2),
+                  paddingTop: hp('2%'),
+                }}>
+                {HomeScreenDetailData.heading}
+              </Text>
+            </View>
+            <View>
+              <Text
+                style={{
+                  color: '#FFFFFF',
+                  fontSize: RFPercentage(3.2),
+                  paddingTop: hp('1.5%'),
+                  paddingBottom: hp('1.5%'),
+                }}>
+                {HomeScreenDetailData.heading2}
+              </Text>
+            </View>
+            <View>
+              <Text
+                style={{
+                  color: '#FFFFFF',
+                  fontSize: RFPercentage(2),
+                }}>
+                {HomeScreenDetailData.heading3}
+              </Text>
+            </View>
+          </View>
+          <View
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'flex-end',
+              paddingRight: wp('5%'),
+              paddingBottom: hp('1%'),
             }}>
+            <Text
+              style={{
+                color: '#F7F8F9',
+                fontSize: RFPercentage(1.8),
+                paddingLeft: wp('5%'),
+                paddingRight: wp('1%'),
+              }}>
+              {HomeScreenDetailData.heading4}
+            </Text>
+
             <Image source={require('../assets/images/icon_movepage.png')} />
-          </TouchableOpacity>
-        </View>
-      </ImageBackground>
+          </View>
+        </ImageBackground>
+      </Pressable>
     </View>
   );
 }
